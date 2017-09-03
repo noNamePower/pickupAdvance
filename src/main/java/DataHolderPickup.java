83afd19ca7f48e54fe7846d4e6e85790ba813670
noNamePower <liooppup@gmail.com>
2017-09-03 12:09:35 +0200

    add maven + DataHolderPickup

--- src/main/java/DataHolderPickup.java
+++ src/main/java/DataHolderPickup.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
-
 public class DataHolderPickup {
+    int kc;
+    int nc;
+    int sets;
+    int date;
+    int lay;
+
+    public void setKc(int kc) {
+        this.kc = kc;
+    }
+
+    public int getKc() {
+        return kc;
+    }
+
+    public void setNc(int nc) {
+        this.nc = nc;
+    }
+
+    public int getNc() {
+        return nc;
+    }
+
+    public void setSets(int sets) {
+        this.sets = sets;
+    }
+
+    public int getSets() {
+        return sets;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setLay(int lay) {
+        this.lay = lay;
+    }
+
+    public int getLay() {
+        return lay;
+    }
 }
